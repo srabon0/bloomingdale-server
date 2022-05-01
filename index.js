78cb5f4578a396b0ba158bb0e6579bb3b45b3bec
srabon0 <srabonemam4@gmail.com>
2022-05-01 14:27:02 +0600

    mongo client added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { config } = require('dotenv');
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qljfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("connected")
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/',(req,res)=>{
     res.send("hello the server is runnig")
